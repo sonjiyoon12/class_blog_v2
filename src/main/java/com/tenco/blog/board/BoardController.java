dd607cc1ae04c7cb35c371b4b06f6fc5dcc1d0e9
sonjiyoon12 <1wldbs1908@gmail.com>
2025-06-23 11:03:49 +0900

    게시글 수정 삭제

--- src/main/java/com/tenco/blog/board/BoardController.java
+++ src/main/java/com/tenco/blog/board/BoardController.java
@@ -16,6 +16,58 @@ public class BoardController {
     // 생성자 의존 주의 - DI 처리
     private final BoardPersistRepository br;
 
+    // 주소 설계 : /board/{{board.id}}/delete
+    @PostMapping("/board/{id}/delete")
+    public String delete(@PathVariable(name = "id") Long id) {
+        br.deleteById(id);
+        return "redirect:/";
+    }
+
+
+    /**
+     * GET 맵핑
+     * 주소 설계 : http://localhost:8080/board/{id}/update-form
+     *
+     * @return update-form.mustache
+     * @Param : id (board pk)
+     */
+    // 게시글 수정하기 화면 요청
+    @GetMapping("/board/{id}/update-form")
+    // 자동 형변환 Long 타입
+    public String updateForm(@PathVariable(name = "id") Long id, HttpServletRequest request) {
+        // select * from board_tb where id = 4;
+        Board board = br.findById(id);
+        // 머스태치 파일에 조회된 데이터를 바인딩 처리
+        request.setAttribute("board", board); // "board" 는 키 값
+        return "board/update-form";
+    }
+
+    // 게시글 수정하기 완료 후 화면 요청
+    @PostMapping("/board/{id}/update-form")
+    // 자동 형변환 Long 타입
+//    public String update(@PathVariable(name = "id") Long id,
+//                         @RequestParam(name = "title") String title,
+//                         @RequestParam(name = "content") String content,
+//                         @RequestParam(name = "username") String username) {
+
+    public String update(@PathVariable(name = "id") Long id,
+                         BoardRequest.UpdateDTO reqDTO) {
+
+        // 트랜잭션
+        // 수정 : select -> 값을 확인해서 -> 데이터 수정 -> update
+        // JPA 영속성 컨텍스트 활용
+        br.update(id, reqDTO);
+        // 수정 전략을 더티 채킹을 활용
+        // 장점
+        // 1. update 쿼리 자동 생성
+        // 2. 변경된 필드만 업데이트(성능 최적화)
+        // 3. 영속성 컨텍스트에 일관성 유지
+        // 4. 1차 캐시 자동 갱신 됨
+
+        // 성공 시 리스트 화면으로 리다이텍트 처리
+        return "redirect:/";
+
+    }
 
 
     // 게시글 상세 보기
@@ -38,7 +90,7 @@ public class BoardController {
     // 1. index.mustache 파일을 반환 시키는 기능을 만든다.
     // 주소 설계 : http://localhost:8080/, http://localhost:8080/index
     @GetMapping({"/index", "/"})
-    public String boardList (HttpServletRequest request) {
+    public String boardList(HttpServletRequest request) {
         List<Board> boardList = br.findAll();
         request.setAttribute("boardList", boardList);
         return "index";
@@ -46,7 +98,7 @@ public class BoardController {
 
     // 게시글 작성 화면 요청 처리
     @GetMapping("/board/save-form")
-    public String saveForm () {
+    public String saveForm() {
         return "board/save-form";
     }
 
--- src/main/java/com/tenco/blog/board/BoardPersistRepository.java
+++ src/main/java/com/tenco/blog/board/BoardPersistRepository.java
@@ -15,11 +15,48 @@ public class BoardPersistRepository {
     // @Autowired - DI 처리 (final를 사용 시 사용 불가)
     private final EntityManager em;
 
+    // 게시글 삭제하기 (영속성 컨텍스트를 활용)
+    @Transactional
+    public void deleteById(Long id) {
+        // 1. 먼저 삭제할 엔티티를 영속 상태로 조회
+        Board board = em.find(Board.class, id);
+
+        // 영속 상태의 엔티티를 삭제 상태로 변경
+        em.remove(board);
+        // 트랜잭션이 커밋 되는 순간 삭제 처리
+
+        // 삭제 과정
+        // 1. Board 엔티티가 영속 상태에서 remove() 호출 시 삭제 상태로 변경
+        // 2. 1차 캐쉬에서 해당 엔티티를 제거
+        // 3. 트랜잭션 커밋 시점에 DELETE SQL 자동 실행
+        // 4. 연관관계 처리 자동 수행 (cascade 설정 시)
+    }
+
+
+    // 게시글 수정하기 (DB 접근 계층)
+    @Transactional
+    public void update(Long boardId, BoardRequest.UpdateDTO updateDTO) {
+
+        Board board = findById(boardId);
+        // board -> 영속성 컨텍스트 1차 캐쉬에 key=vale 값이 저장 되어 있다.
+        board.setTitle(updateDTO.getTitle());
+        board.setContent(updateDTO.getContent());
+        board.setUsername(updateDTO.getUsername());
+
+        // 트랜잭션 끝나면 영속성 컨텍스트에서 변경 감지를 한다.
+        // 변경 감지(Dirty Checking)
+        // 1. 영속성 컨텍스트가 엔티티 최초 상태를 스냅샷으로 보관
+        // 2. 필드 값 변경 시 현재 상태와 스냅샷 비교
+        // 3. 트랜잭션 커밋 시점에 변경된 필드만 UPDATE 쿼리를 자동 생성
+        // 4. Update board_tb set title =?, content=?, username=? where id =?
+
+    }
+
     // 게시글 한건 조회 쿼리 만들기
     // em.find(), JPQL, 네이티브 쿼리
     public Board findById(Long id) {
         // 1차 캐시 활용
-//        Board board = em.find(Board.class, id);
+//        Board board = em.find(Board.class, id); // 영속성 상태로 조회
 //        return board;
 
         return em.find(Board.class, id);
